4c9dd6bd61585ccfd3bfcce745da7de33a1498b6
46530 <46530@10-600-07>
2022-03-28 10:59:55 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lresult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbSource
+            // 
+            this.tbSource.Location = new System.Drawing.Point(294, 174);
+            this.tbSource.Name = "tbSource";
+            this.tbSource.Size = new System.Drawing.Size(242, 20);
+            this.tbSource.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(275, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "введите послед со скобками";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(379, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Проверить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lresult
+            // 
+            this.lresult.AutoSize = true;
+            this.lresult.Location = new System.Drawing.Point(404, 283);
+            this.lresult.Name = "lresult";
+            this.lresult.Size = new System.Drawing.Size(0, 13);
+            this.lresult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lresult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSource);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lresult;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<char, char> skobki = new Dictionary<char, char>()
+        {
+            { ')','(' },
+            { ']','[' },
+            { '}','{' }
+        };
+        string openskobki = "([{", closeskobki = ")]}";
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stack<char> mystack = new Stack<char>();
+            bool isOK = true;
+            char temp;
+            foreach (var c in tbSource.Text)
+            {
+                if (openskobki.Contains(c)) mystack.Push(c);
+                if(closeskobki.Contains(c))
+                {
+                    if (mystack.Count == 0 || mystack.Peek() != skobki[c])
+                    {
+                        isOK = false;
+                        break;
+                    }
+                    else
+                    {
+                       mystack.Pop();
+                       
+                    }
+                }
+
+            }
+            if (!isOK || mystack.Count > 0) lresult.Text = "Неверно";
+            else lresult.Text = "МегаХорош";
+        }
+    }
+}
